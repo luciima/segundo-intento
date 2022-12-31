ba6b3aa122ef2607d64581e28a233976ef643d9c
luciima <luciiamar19@gmail.com>
2022-12-31 17:00:43 -0300

    Crea las funciones para calcular lo pedido
    Lo pedido por la consigna: promedio, número mayor, número menor, número que más aparece

---
+++ tareas/clase-5-4/clase-5-4.js
@@ -0,0 +1,58 @@
+//TAREA: En otro archivo distinto,
+// Crear una lista de <ol> y <li> que contengan sólo números.
+// Convertir esos números a un array y:
+// 1. calcular el promedio y mostrarlo en un <em> pre-creado con el texto "El promedio es..."
+// 2. obtener el número más pequeño y mostrarlo en un <em> pre-creado con el texto "El número más pequeño es..."
+// 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
+// 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
+
+function calcularPromedio(numeros) {
+    let sum = 0;
+    for (let numero of numeros) {
+        sum += Number(numero);
+    }
+    return sum / numeros.length;
+}
+
+function encontrarMenorNumero(numeros) {
+    let menorNumero = numeros[0];
+    for (let numero of numeros) {
+        if (numero < menorNumero) {
+            menorNumero = numero;
+        }
+    }
+    return menorNumero;
+}
+
+function encontrarMayorNumero(numeros) {
+    let mayorNumero = numeros[0];
+    for (let numero of numeros) {
+        if (numero > mayorNumero) {
+            mayorNumero = numero;
+        }
+    }
+    return mayorNumero;
+}
+
+function contarApariciones(numero, lista) {
+    let vecesQueAparece = 0;
+    for (let elemento of lista) {
+        if (numero === elemento) {
+            vecesQueAparece += 1;
+        }
+    }
+    return vecesQueAparece;
+}
+
+function encontrarNumeroMasFrecuente(numeros) {
+    let numeroMasFrecuente = numeros[0];
+    let vecesQueAparece = contarApariciones(numeros[0], numeros);
+    for (let numero of numeros) {
+        if (contarApariciones(numero, numeros) > vecesQueAparece) {
+            numeroMasFrecuente = numero;
+            vecesQueAparece = contarApariciones(numero, numeros);
+        }
+    }
+    return numeroMasFrecuente;
+}
+
